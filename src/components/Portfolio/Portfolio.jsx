662d03eca54128488b13d26075128c70bbb34a6b
Aleksandr <sanyamaks14@gmail.com>
2019-04-03 23:55:56 +0400

    добавлен react-stack-grid

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,41 +1,55 @@
 import React, {Component} from 'react';
+import StackGrid, {transitions, easings} from "react-stack-grid"
 import Portfolio1 from '../../img/Portfolio1.jpg'
 import Portfolio2 from '../../img/Portfolio2.jpg'
 import Portfolio3 from '../../img/Portfolio3.jpg'
 import Portfolio4 from '../../img/Portfolio4.jpg'
 
+const transition = transitions.scaleDown;
+
+
+
 class Portfolio extends Component {
     render() {
         return (
-            <main className="main">
-
-                <div className="main_img">
-                    <img src={Portfolio1} alt="1-ое изображение"/>
-                </div>
-                <div className="main_img">
-                    <img src={Portfolio2} alt="2-ое изображение"/>
-                </div>
-                <div className="main_img">
-                    <img src={Portfolio3} alt="3-ое изображение"/>
-                </div>
-                <div className="main_img">
-                    <img src={Portfolio4} alt="4-ое изображение"/>
-                </div>
-                <div className="main_p">
-                    <p>
-                        Основной текст, который будет описывать бренд
-                        < nobr> M A R I N. A R T.</nobr>
-                        Не обязательно много , скорее даже мало, но круто и пафосно.При этои я добвлю здесь текст, чтобы
-                        проверить, пушится ли с ноута.
-                        Основной текст, который будет описывать бренд < nobr> M A R I N. A R T.</nobr> Не обязательно
-                        много , скорее даже мало, но круто и пафосно.При этои я добвлю здесь текст, чтобы проверить,
-                        пушится ли с ноута.
-                        Основной текст, который будет описывать бренд <nobr> M A R I N. A R T.</nobr> Не обязательно
-                        много , скорее даже мало, но круто и пафосно.При этои я добвлю здесь текст, чтобы проверить,
-                        пушится ли с ноута.
-                    </p>
-                </div>
-
+            <main className="gallery">
+                <StackGrid monitorImagesLoaded
+                           columnWidth={200}
+                           duration={600}
+                           gutterWidth={15}
+                           gutterHeight={15}
+                           easing={easings.cubicOut}
+                           appearDelay={60}
+                           appear={transition.appear}
+                           appeared={transition.appeared}
+                           enter={transition.enter}
+                           entered={transition.entered}
+                           leaved={transition.leaved}>
+                    <div className="gallery__image">
+                        <img src={Portfolio1} alt="1-ое изображение"/>
+                    </div>
+                    <div className="gallery__image">
+                        <img src={Portfolio2} alt="2-ое изображение"/>
+                    </div>
+                    <div className="gallery__image">
+                        <img src={Portfolio3} alt="3-ое изображение"/>
+                    </div>
+                    <div className="gallery__image">
+                        <img src={Portfolio4} alt="4-ое изображение"/>
+                    </div>
+                    <div className="gallery__image">
+                        <img src={Portfolio1} alt="1-ое изображение"/>
+                    </div>
+                    <div className="gallery__image">
+                        <img src={Portfolio2} alt="2-ое изображение"/>
+                    </div>
+                    <div className="gallery__image">
+                        <img src={Portfolio3} alt="3-ое изображение"/>
+                    </div>
+                    <div className="gallery__image">
+                        <img src={Portfolio4} alt="4-ое изображение"/>
+                    </div>
+                </StackGrid>
 
             </main>
         );
