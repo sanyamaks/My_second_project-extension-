732e091e5a2ab37148f936ee1a680bfee9e80707
Aleksandr <sanyamaks14@gmail.com>
2019-04-23 00:20:26 +0400

    Загрузил все информацию о картинках в портфолио в массив. Теперь всё подтягивается из него

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -10,6 +10,28 @@ import Portfolio4 from '../../img/Portfolio4.jpg'
 const transition = transitions.scaleDown;
 
 
+
+const GalleryData = [
+{nameGalleryImage: Portfolio1, nameAlt: '1-ое изображение', nameGalleryTitle:'Название картинки', nameGalleryDescription:'Описание картинки'},
+{nameGalleryImage: Portfolio2, nameAlt: '2-ое изображение', nameGalleryTitle:'Название картинки', nameGalleryDescription:'Описание картинки'},
+{nameGalleryImage: Portfolio3, nameAlt: '3-ое изображение', nameGalleryTitle:'Название картинки', nameGalleryDescription:'Описание картинки'},
+{nameGalleryImage: Portfolio4, nameAlt: '4-ое изображение', nameGalleryTitle:'Название картинки', nameGalleryDescription:'Описание картинки'},
+{nameGalleryImage: Portfolio1, nameAlt: '5-ое изображение', nameGalleryTitle:'Название картинки', nameGalleryDescription:'Описание картинки'},
+{nameGalleryImage: Portfolio2, nameAlt: '6-ое изображение', nameGalleryTitle:'Название картинки', nameGalleryDescription:'Описание картинки'},
+{nameGalleryImage: Portfolio3, nameAlt: '7-ое изображение', nameGalleryTitle:'Название картинки', nameGalleryDescription:'Описание картинки'},
+{nameGalleryImage: Portfolio4, nameAlt: '8-ое изображение', nameGalleryTitle:'Название картинки', nameGalleryDescription:'Описание картинки'}
+
+]
+
+let GalleryElement = GalleryData.map(el => {return <GalleryPart
+    nameGalleryImage={el.nameGalleryImage}
+    nameAlt={el.nameAlt}
+    nameGalleryTitle={el.nameGalleryTitle}
+    nameGalleryDescription={el.nameGalleryDescription}
+/>})
+
+
+
 class Portfolio extends Component {
     render() {
         return (
@@ -29,61 +51,7 @@ class Portfolio extends Component {
                            enter={transition.enter}
                            entered={transition.entered}
                            leaved={transition.leaved}>
-                    <GalleryPart
-                    nameGalleryImage = {Portfolio1}
-                    nameAlt = '1-ое изображение'
-                    nameGalleryTitle = 'Название картинки'
-                    nameGalleryDescription = 'Описание картинки'
-                    />
-
-                    <GalleryPart
-                        nameGalleryImage = {Portfolio2}
-                        nameAlt = '2-ое изображение'
-                        nameGalleryTitle = 'Название картинки'
-                        nameGalleryDescription = 'Описание картинки'
-                    />
-
-                    <GalleryPart
-                        nameGalleryImage = {Portfolio3}
-                        nameAlt = '3-ое изображение'
-                        nameGalleryTitle = 'Название картинки'
-                        nameGalleryDescription = 'Описание картинки'
-                    />
-
-                    <GalleryPart
-                        nameGalleryImage = {Portfolio4}
-                        nameAlt = '4-ое изображение'
-                        nameGalleryTitle = 'Название картинки'
-                        nameGalleryDescription = 'Описание картинки'
-                    />
-
-                    <GalleryPart
-                        nameGalleryImage = {Portfolio1}
-                        nameAlt = '5-ое изображение'
-                        nameGalleryTitle = 'Название картинки'
-                        nameGalleryDescription = 'Описание картинки'
-                    />
-
-                    <GalleryPart
-                        nameGalleryImage = {Portfolio2}
-                        nameAlt = '6-ое изображение'
-                        nameGalleryTitle = 'Название картинки'
-                        nameGalleryDescription = 'Описание картинки'
-                    />
-
-                    <GalleryPart
-                        nameGalleryImage = {Portfolio3}
-                        nameAlt = '7-ое изображение'
-                        nameGalleryTitle = 'Название картинки'
-                        nameGalleryDescription = 'Описание картинки'
-                    />
-
-                    <GalleryPart
-                        nameGalleryImage = {Portfolio4}
-                        nameAlt = '8-ое изображение'
-                        nameGalleryTitle = 'Название картинки'
-                        nameGalleryDescription = 'Описание картинки'
-                    />
+                    {GalleryElement}
                 </StackGrid>
 
             </main>
